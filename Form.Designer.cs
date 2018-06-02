c24f6c94fa6febddb50a45b8849ef9cdb83ac71a
RamKumar1985 <ramvashista85@gmail.com>
2018-06-02 18:20:40 +0530

    Add files via upload

---
+++ Form.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnDrawShape = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnDrawShape
+            // 
+            this.btnDrawShape.Location = new System.Drawing.Point(613, 12);
+            this.btnDrawShape.Name = "btnDrawShape";
+            this.btnDrawShape.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawShape.TabIndex = 1;
+            this.btnDrawShape.Text = "Draw Shape";
+            this.btnDrawShape.UseVisualStyleBackColor = true;
+            this.btnDrawShape.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "lblErrorMessage";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 749);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDrawShape);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnDrawShape;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var text = textBox1.Text;
+            if (string.IsNullOrEmpty(text))
+            {
+                label1.Text = "String is not valid";
+            }
+            else
+            {
+                text = text.Trim().ToLower();
+                var splitText = text.Split(' ');
+                var shape = splitText[2];
+
+                string measurement = string.Empty;
+                string amount1 = string.Empty;
+                string amount2 = string.Empty;
+                Dictionary<string, string> values = new Dictionary<string, string>();
+
+                switch (shape)
+                {
+                    case "circle":
+                        //measurement = splitText[5];
+                        //amount1 = splitText[7];
+                        if (splitText.Count() == 8)
+                        {
+                            values.Add(splitText[5], splitText[7]);
+                        }
+                        else
+                        {
+                            label1.Text = "Entered string is not in correct format" + "\n" + "E.g.: Draw a circle with a radius of 100";
+                        }
+                        break;
+                    case "square":
+                        //measurement = "side";
+                        //amount1 = splitText[8];
+                        if (splitText.Count() == 9)
+                            values.Add("side", splitText[8]);
+                        else
+                            label1.Text = "Entered string is not in correct format" + "\n" + "E.g.: Draw a square with a side length of 200";
+                        break;
+                    case "rectangle":
+                        if (splitText.Count() == 13)
+                        {
+                            int index = 0;
+                            foreach (var item in splitText)
+                            {
+                                index += 1;
+                                if (item == "width")
+                                {
+                                    values.Add("width", splitText[index + 1]);
+                                    //amount1 = splitText[index + 1];
+                                }
+                                else if (item == "height")
+                                {
+                                    //amount2 = splitText[index + 1];
+                                    values.Add("height", splitText[index + 1]);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            label1.Text = "Entered string is not in correct format" + "\n" + "E.g.: Draw a rectangle with a width of 250 and a height of 400";
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                drawShape(shape, values);
+            }
+
+        }
+
+        private bool drawShape(string shape, Dictionary<string, string> dict)
+        {
+            if (dict.Count > 0)
+            {
+                var checkAmount = dict.Select(pair => pair.Value).FirstOrDefault();
+                if (!IsNumeric(checkAmount))
+                {
+                    label1.Text = "Amount is invalid";
+                    return false;
+                }
+                else
+                {
+                    if (shape == "circle")
+                    {
+                        #region paramerter                        
+                        if (dict.ContainsKey("radius"))
+                        {
+                            if (IsNumeric(dict["radius"]))
+                            {
+                                int value = Convert.ToInt32(dict["radius"]);
+                                drawCircle(value);
+                            }
+                            else
+                            {
+                                label1.Text = "Amount is invalid";
+                                return false;
+                            }
+                        }
+
+                        #endregion
+
+                    }
+                    else if (shape == "square")
+                    {
+                        #region parmeter
+
+
+                        if (dict.ContainsKey("side"))
+                        {
+                            if (IsNumeric(dict["side"]))
+                            {
+                                int value = Convert.ToInt32(dict["side"]);
+
+                                drawRectange(value, value);
+                            }
+                            else
+                            {
+                                label1.Text = "Amount is invalid";
+                                return false;
+                            }
+                        }
+                        #endregion
+                    }
+                    else if (shape == "rectangle")
+                    {
+                        int height;
+                        int width;
+
+                        #region parameter                        
+                        if (dict.ContainsKey("width") && dict.ContainsKey("height"))
+                        {
+                            if (IsNumeric(dict["width"]))
+                            {
+                                width = Convert.ToInt32(dict["width"]);
+                            }
+                            else
+                            {
+                                label1.Text = "Amount is invalid";
+                                return false;
+                            }
+
+                            if (IsNumeric(dict["height"]))
+                            {
+                                height = Convert.ToInt32(dict["height"]);
+                            }
+                            else
+                            {
+                                label1.Text = "Amount is invalid";
+                                return false;
+                            }
+
+                        }
+                        else
+                        {
+                            label1.Text = "Amount is invalid";
+                            return false;
+                        }
+                        #endregion
+                        label1.Text = "Width: " + width + "Height: " + height;
+
+                        drawRectange(height, width);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private void drawRectange(int height, int width)
+        {
+            Graphics myGraphics = base.CreateGraphics();
+            myGraphics.Clear(Color.White);
+            // Create pen.
+            Pen blackPen = new Pen(Color.Red, 3);
+            // Create rectangle.
+            Rectangle rect = new Rectangle(100, 200, width, height);
+            // Draw rectangle to screen.
+            myGraphics.DrawRectangle(blackPen, rect);
+            label1.Text = "";
+        }
+
+        private void drawCircle(int value)
+        {
+            Graphics myGraphics = base.CreateGraphics();
+            myGraphics.Clear(Color.White);
+
+            Pen myPen = new Pen(Color.Red);
+            SolidBrush mySolidBrush = new SolidBrush(Color.Red);
+            myGraphics.DrawEllipse(myPen, value, value, value, value);
+            label1.Text = "";
+        }
+
+        public bool IsNumeric(string input)
+        {
+            int test;
+            return int.TryParse(input, out test);
+        }
+    }
+}
